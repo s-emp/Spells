3dfd9ae7c3fa760e9364978f88d2451aece2d423
Sergey Melnikov <sergey.melnikov@sl-tech.ru>
2019-09-19 17:24:59 +0300

    Next

--- Spells/Spells/Core/ViewController/PopupVC/PopupMenuVC.swift
+++ Spells/Spells/Core/ViewController/PopupVC/PopupMenuVC.swift
@@ -10,12 +10,37 @@ import UIKit
 
 class PopupMenuVC: UIViewController {
     
-    var rootView: UIView!
+    private let rootView: UIView = UIApplication.shared.windows.first!.rootViewController!.view!
+    
     var startRect: CGRect?
     var endRect: CGRect!
-    var titlePopupMenu: String?
+    var childVC: UIViewController?
     
-    var popupMenu: UIView!
+    private var popupMenu: UIView!
+    
+    func show() {
+        guard let childVC = childVC else { return }
+        addChild(childVC)
+        view.frame = rootView.bounds
+        popupMenu.frame = startRect ?? CGRect(origin: rootView.center, size: CGSize(width: 40, height: 40))
+        childVC.view.frame = popupMenu.bounds
+        popupMenu.addSubview(childVC.view)
+        rootView.addSubview(view)
+        UIView.animate(withDuration: 0.2) {
+            self.view.alpha = 1
+            self.popupMenu.frame = self.endRect
+            childVC.view.frame = self.popupMenu.bounds
+            self.popupMenu.alpha = 1
+        }
+    }
+    
+    func hide() {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.view.alpha = 0
+            self.popupMenu.frame = self.startRect ?? .zero
+            self.popupMenu.alpha = 0
+        })
+    }
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -32,17 +57,16 @@ class PopupMenuVC: UIViewController {
         view.alpha = 0
         view.backgroundColor = UIColor(named: .popupBackground)
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
 }
 
 // MARK: - Prepare
 extension PopupMenuVC {
     fileprivate func preparePopupMenu() {
         popupMenu = UIView(frame: .zero)
+        popupMenu.clipsToBounds = true
+        popupMenu.alpha = 0
         popupMenu.backgroundColor = UIColor(named: .background)
         popupMenu.layer.cornerRadius = 20
+        view.addSubview(popupMenu)
     }
 }
