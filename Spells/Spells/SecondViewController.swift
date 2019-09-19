fe5e802a432c2aed87727a819917c3d0280dc4de
Sergey Melnikov <sergey.melnikov@sl-tech.ru>
2019-09-19 11:30:29 +0300

    Init project

---
+++ Spells/Spells/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Spells
+//
+//  Created by Sergey Melnikov on 19/09/2019.
+//  Copyright © 2019 Sergey Melnikov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
